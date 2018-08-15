cb473f959498a2329054ea151e08bc5130987d41
sal.abbasi <abbasi.sal@gmail.com>
2018-08-15 14:50:05 -0500

    upd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../..'))
+
 
 #autodoc_default_flags = {'special-members': '__init__'}
 napoleon_include_init_with_doc = True
