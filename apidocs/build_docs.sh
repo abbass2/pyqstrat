e705e28384bd8234ffa84617b1160f27193b280d
sal.abbasi <abbasi.sal@gmail.com>
2018-10-23 18:24:19 -0500

    upd

--- apidocs/build_docs.sh
+++ apidocs/build_docs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# Next line is commented because I write my own pyqstrat.rst file in the source directory instead of having sphinx-apidoc create it.
-# It screws up on cpp code and the example submodule
-# sphinx-apidoc -f -o ./source/ .. ../setup.py
+
 set -x
-make clean
-make html
+cp ../build/lib.macosx-10.9-x86_64-3.6/pyqstrat/pyqstrat_cpp.cpython-36m-darwin.so ../pyqstrat/
+rm -Rf build/
+sphinx-build -M html "source" "build"
