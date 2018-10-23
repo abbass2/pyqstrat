bae6b43d24d94fe00a43ca33c30a17119e784e9e
sal.abbasi <abbasi.sal@gmail.com>
2018-10-23 18:31:54 -0500

    upd

--- apidocs/build_docs.sh
+++ apidocs/build_docs.sh
@@ -2,5 +2,7 @@
 
 set -x
 cp ../build/lib.macosx-10.9-x86_64-3.6/pyqstrat/pyqstrat_cpp.cpython-36m-darwin.so ../pyqstrat/
-rm -Rf build/
+rm -Rf ./build/
 sphinx-build -M html "source" "build"
+rm -Rf ../docs/*
+cp -R ./build/html/* ../docs/
