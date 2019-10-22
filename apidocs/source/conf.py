5a04347095da8b66a6d9688f021928ed4dc48543
sal.abbasi <abbasi.sal@gmail.com>
2019-10-22 17:17:51 -0500

    upd

--- apidocs/source/conf.py
+++ apidocs/source/conf.py
@@ -32,7 +32,10 @@ napoleon_include_init_with_doc = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.napoleon',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.autodoc',
+              'sphinx_autodoc_typehints']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
