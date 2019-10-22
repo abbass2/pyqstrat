aad30bed8f94fe86c9378bc591e51bcaf518243b
sal.abbasi <abbasi.sal@gmail.com>
2019-10-21 20:35:08 -0500

    upd

--- pyqstrat/src_nb/generate_py_files.sh
+++ pyqstrat/src_nb/generate_py_files.sh
@@ -6,8 +6,7 @@ do
     tmpname="$newname.tmp"
     echo processing "$file" to "$newname"
     jq -j ' .cells | map( select(.cell_type == "code") | .source + ["\n\n"] ) | .[][]' "$file" > "$tmpname"
-    grep -v -e '^%' "$tmpname" | grep -v "get_ipython()" > "$newname"
+    grep -v -e '^%' "$tmpname" | grep -v "get_ipython()" > "../$newname"
     rm $tmpname
 done
-mv *.py ../
 
