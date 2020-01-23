35a7fff9aaeec937d9d5abcfe254d6e32f5ea2a3
= <abbasi.sal@gmail.com>
2020-01-22 19:27:51 -0600

    updated

---
+++ pyqstrat/src_nb/gen_py_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $1 
+for file in *.ipynb
+do
+    newname="${file%.ipynb}.py"
+    tmpname="$newname.tmp"
+    echo processing "$file" to "$newname"
+    jq -j ' .cells | map( select(.cell_type == "code") | .source + ["\n\n"] ) | .[][]' "$file" > "$tmpname"
+    # remove ipython magics and trailing blank lines
+    grep -v -e '^%' "$tmpname" | grep -v "get_ipython()" | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' > "$newname"
+    # Delete all trailing blank lines at end of file (only).
+    rm $tmpname
+done
+cd -
