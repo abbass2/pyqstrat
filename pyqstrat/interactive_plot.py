b8135df073b78057d5336cc5dce64bc538ea96c8
sal.abbasi <abbasi.sal@gmail.com>
2021-08-26 17:07:47 -0500

    updated

--- pyqstrat/interactive_plot.py
+++ pyqstrat/interactive_plot.py
@@ -550,4 +550,3 @@ if __name__ == '__main__':
     doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
     print('done')
-
