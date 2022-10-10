e59659342b560dca46340a6117a4fbc94d3eae6f
sal <abbasi.sal@gmail.com>
2022-10-10 12:01:14 -0500

    updated

--- pyqstrat/cpp/io/read_file.cpp
+++ pyqstrat/cpp/io/read_file.cpp
@@ -16,6 +16,34 @@
 #include "csv_reader.hpp"
 #include "numpy/ndarrayobject.h"
 
+
+#include <time.h>
+#include <iomanip>
+#include <sstream>
+
+
+//strptime not inmplemented in windows
+#ifdef _MSC_VER
+extern "C" char* strptime(const char* s,
+                          const char* f,
+                          struct tm* tm) {
+  // Isn't the C++ standard lib nice? std::get_time is defined such that its
+  // format parameters are the exact same as strptime. Of course, we have to
+  // create a string stream first, and imbue it with the current C locale, and
+  // we also have to make sure we return the right things if it fails, or
+  // if it succeeds, but this is still far simpler an implementation than any
+  // of the versions in any of the C standard libraries.
+  std::istringstream input(s);
+  input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+  input >> std::get_time(tm, f);
+  if (input.fail()) {
+    return nullptr;
+  }
+  return (char*)(s + input.tellg());
+}
+#endif
+
+
 using namespace std;
 
 static int read_list(PyObject* list, vector<int>& vec) {
