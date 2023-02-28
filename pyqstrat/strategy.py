7f55f1fa4ba1f36df2ea3a81cf1fbb31e7f1fc5a
sal <abbasi.sal@gmail.com>
2023-02-28 13:52:14 -0600

    updated

--- pyqstrat/strategy.py
+++ pyqstrat/strategy.py
@@ -109,8 +109,8 @@ class Strategy:
                 If not set, the __init__ function will create an empty member strategy_context object that you can access.
         '''
         self.name = 'main'  # Set by portfolio when running multiple strategies
-        increasing_ts: bool = all(np.diff(timestamps.astype(int)) > 0)
-        print(increasing_ts, type(increasing_ts))
+        increasing_ts: bool = bool(np.all(np.diff(timestamps.astype(int)) > 0))
+        # print(increasing_ts, type(increasing_ts))
         assert_(increasing_ts, f'timestamps must be monotonically increasing: {timestamps[:100]} ...')
         self.timestamps = timestamps
         assert_(len(contract_groups) > 0 and isinstance(contract_groups[0], ContractGroup))
