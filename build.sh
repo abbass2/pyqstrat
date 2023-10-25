0169cb9e65af1280cd150ffcbfa7962ec609ab63
sal <abbasi.sal@gmail.com>
2023-10-24 22:14:39 -0500

    updated

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -x
-find pyqstrat -maxdepth 1 -name '*.ipynb' -exec jup_mini {} \;
+set -e
+# generate .py files for notebooks and quit on first error
+find pyqstrat -maxdepth 1 -name '*.ipynb' \( -exec jup_mini {} \; -o -quit \)
+
 export NO_DISPLAY=1
 mypy --ignore-missing-imports pyqstrat/
 flake8 --ignore W291,W293,W503,E402,E701,E275,E741 --max-line-length=160 --extend-exclude notebooks pyqstrat/
-# run notebooks
-find pyqstrat/notebooks . -maxdepth 1 -name '*.ipynb' \( -exec ipython {} \; -o -quit \) 
 
-set -x
+# run notebooks and exit on first error
+find pyqstrat/notebooks -name '*.ipynb' \( -exec sh -c 'trap "exit \$?" EXIT; ipython "$0"' {} \; -o  -quit \)
+
 # build docs
 rm -Rf ./docs/*
 sphinx-build ./apidocs/source ./docs/
-# rm -Rf ./docs/build/
+rm -Rf ./docs/build/
+set +e
 set +x
